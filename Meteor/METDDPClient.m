9c2337ce08639d3ee3fc1f5eb460661d0ca575e3
Seth Sandler <cerupcat@gmail.com>
2017-09-25 15:01:52 -0700

    Fix a BOOL return that should be void

--- Meteor/METDDPClient.m
+++ Meteor/METDDPClient.m
@@ -278,7 +278,7 @@ NSString * const METDDPClientDidChangeAccountNotification = @"METDDPClientDidCha
     }
 }
 
-- (BOOL)networkReachabilityManagerForStatus:(METNetworkReachabilityStatus)reachabilityStatus NS_EXTENSION_UNAVAILABLE_IOS("applicationDidEnterBackground not available in extension")
+- (void)networkReachabilityManagerForStatus:(METNetworkReachabilityStatus)reachabilityStatus NS_EXTENSION_UNAVAILABLE_IOS("applicationDidEnterBackground not available in extension")
 {
     if ([UIApplication sharedApplication].applicationState != UIApplicationStateBackground)  {
         [self connect];
